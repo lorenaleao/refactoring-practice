da584a0bf933b02100b3c198d3c93ee7ae39a257
lorenaleao <lleao@cyral.com>
2020-10-20 22:36:37 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -41,10 +41,6 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        // there is a bonus for a two day new release rental
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) 
-            return 2;
-        else
-            return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
