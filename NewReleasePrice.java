2a90b5c45e843deda8144cd812cb7d21d0254b76
lorenaleao <lleao@cyral.com>
2020-10-20 22:39:51 -0300

    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -6,4 +6,10 @@ public class NewReleasePrice extends Price {
     public double getCharge(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        // there is a bonus for a two day new release rental
+        return (daysRented > 1) ? 2 : 1;
+    }
 }
--- Price.java
+++ Price.java
@@ -3,10 +3,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
     
     public int getFrequentRenterPoints(int daysRented) {
-        // there is a bonus for a two day new release rental
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) 
-            return 2;
-        else
-            return 1;
+        return 1;
     }
 }
